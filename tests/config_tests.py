8d66a52bf035cfb3d7770b76d4d9fa6ab26ca6e4
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2020-10-20 01:41:43 -0700

    Add tests for large values of particulates

--- tests/config_tests.py
+++ tests/config_tests.py
@@ -10,11 +10,13 @@ class TestConfig(unittest.TestCase):
         """Test whether get_range() behaves as expected.
 
         Tests the following:
-        - that the lower bound for both pm2.5 and pm10 works
-        - that a very high number should be labeled 'Hazardous'
-        - that pm must be 2.5 or 10 AND an error should be logged
+
+            1. That the lower bound for both pm2.5 and pm10 works
+            2. That a very high number should be labeled 'Hazardous'
+            3. That pm must be 2.5 or 10 AND an error should be logged
 
         """
+        # 1.
         self.assertEqual(
             config.get_range(2.5, 0),
             config.asdict(config.RANGES[2.5][0])
@@ -23,6 +25,18 @@ class TestConfig(unittest.TestCase):
             config.get_range(10, 355),
             config.asdict(config.RANGES[10][4])
             )
+
+        # 2.
+        self.assertEqual(
+            config.get_range(2.5, config._AQ_PM_RANGES[2.5][-1] + 1)['label'],
+            'Hazardous'
+            )
+        self.assertEqual(
+            config.get_range(10, config._AQ_PM_RANGES[10][-1] + 1)['label'],
+            'Hazardous'
+            )
+
+        # 3.
         with self.assertLogs(logger=config.LOGGER, level=config.logging.ERROR):
             self.assertRaises(KeyError, config.get_range, 3, 0)
 
