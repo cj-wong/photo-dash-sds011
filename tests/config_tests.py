d2c28fee3b3c64a3ca4a5417943bb7f8f06fd8da
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2020-10-20 01:38:21 -0700

    Add more tests for config.get_range()

--- tests/config_tests.py
+++ tests/config_tests.py
@@ -10,10 +10,21 @@ class TestConfig(unittest.TestCase):
         """Test whether get_range() behaves as expected.
 
         Tests the following:
-        - that pm must be 2.5 or 10
+        - that the lower bound for both pm2.5 and pm10 works
+        - that a very high number should be labeled 'Hazardous'
+        - that pm must be 2.5 or 10 AND an error should be logged
 
         """
-        self.assertRaises(KeyError, config.get_range, 3, 0)
+        self.assertEqual(
+            config.get_range(2.5, 0),
+            config.asdict(config.RANGES[2.5][0])
+            )
+        self.assertEqual(
+            config.get_range(10, 355),
+            config.asdict(config.RANGES[10][4])
+            )
+        with self.assertLogs(logger=config.LOGGER, level=config.logging.ERROR):
+            self.assertRaises(KeyError, config.get_range, 3, 0)
 
 
 if __name__ == '__main__':
